58d8cf7b7a8156920273001fcaae1fbe224ae3b6
and4nte <86049582+and4nte@users.noreply.github.com>
2022-12-28 18:14:32 +0900

    [level 1] Title: 삼총사, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.js
@@ -0,0 +1,12 @@
+function solution(number) {
+    let trios = 0;
+
+    for (let i = 0; i < number.length - 2; i++) {
+        for (let j = i+1; j < number.length - 1; j++) {
+            for (let k = j+1; k < number.length; k++) {
+                number[i] + number[j] + number[k] === 0 && trios++;
+            }
+        }
+    }
+    return trios;
+}
