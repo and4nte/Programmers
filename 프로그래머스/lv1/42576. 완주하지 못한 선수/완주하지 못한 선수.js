cf2f3e1935d16bc6f26d523601c0ee22b28f87d5
and4nte <86049582+and4nte@users.noreply.github.com>
2023-01-05 07:20:55 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 32.95 ms, Memory: 63.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -0,0 +1,17 @@
+function solution(participant, completion) {
+    const map = new Map();
+    
+    for (let i = 0; i < completion.length; i++) {
+        map.set(completion[i], (map.get(completion[i]) || 0) + 1);
+    }
+    
+    for (let i = 0; i < participant.length; i++) {
+        const current = map.get(participant[i]);
+        if (current > 0) {
+            map.set(participant[i], current - 1);
+            continue;
+        }
+        
+        return participant[i];
+    }
+}
